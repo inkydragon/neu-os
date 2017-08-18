c474d0868c8cda4124e93a78395960b04d8ad6bb
0u0 <wo@wo-class.cn>
2017-08-18 18:22:29 +0800

    [Lesson 0x02 Code in Video] Load floppy disk into RAM

--- Makefile
+++ Makefile
@@ -13,8 +13,18 @@ bootsect: bootsect.s ld-bootsect.ld
 	@$(LD) $(LDFLAGS) -o bootsect bootsect.o
 	@objcopy -O binary -j .text bootsect
 
-run: bootsect
-	@$(QEMU) -boot a -fda bootsect
+run: Image
+	@$(QEMU) -boot a -fda Image
+
+Image: bootsect demo
+	@dd if=bootsect of=Image bs=512 count=1
+	@dd if=demo of=Image bs=512 count=1 seek=1
+	@echo "Image build!"
+
+demo: demo.S ld-bootsect.ld
+	@$(AS) -o demo.o demo.S
+	@$(LD) $(LDFLAGS) -o demo demo.o
+	@objcopy -O binary -j .text demo
 
 clean:
-	@rm -f bootsect *.o
+	@rm -f bootsect demo *.o
--- bootsect.s
+++ bootsect.s
@@ -1,80 +1,103 @@
 #################################################################
 #                                                               #
-#  Lesson 1: 创建一个"Hello World"引导扇                        #
-#  Goal: 了解操作系统引导的过程，并且通过使用实模式下的汇       #
-#  在屏幕上输出一行字符串并进入到死循环                         #
+#  Lesson 2: 加载软盘中的内容到内存，并执行相应代码             #
+#  Goal: 了解 int 0x13 的使用方法，以及了解如何使用ljmp切换     #
+#  CS 和IPn                                                     #
 #                                                               #
 #################################################################
 
-.code16   # 指定语法为 十六位汇编
+.code16  # 指定语法为 十六位汇编
 
 # rewrite with AT&T syntax by falcon <wuzhangjin@gmail.com> at 081012
 # Modified by VOID001<zhangjianqiu13@gmail.com> at 2017 03 05
 # loads pretty fast by getting whole sectors at a time whenever possible.
 
-.global _bootstart  # 程序开始处
+.global _bootstar   # 程序开始处
+
+# 当此扇区被BIOS识别为启动扇区装载到内存中时，装载到0x07c0段处
+# 此时我们处于实模式(REAL MODE)中，对内存的寻址方式为
+# (段地址 << 4 + 偏移量) 可以寻址的线性空间为 20 位
+.equ BOOTSEG, 0x07c0  
+.equ INITSEG, 0x9000 # 存放初始化参数
+.equ DEMOSEG, 0x1000
 
 .text
 
-.equ BOOTSEG, 0x07c0  
-     # 当此扇区被BIOS识别为启动扇区装载到内存中时，装载到0x07c0段处
-     # 此时我们处于实模式(REAL MODE)中，对内存的寻址方式为
-     # (段地址 << 4 + 偏移量) 可以寻址的线性空间为 20 位
-
-ljmp $BOOTSEG, $_bootstart   
+ljmp $BOOTSEG, $_bootstart 
 # 修改cs寄存器为BOOTSEG, 并跳转到_start处执行我们的代码
 # 清理掉流水线缓存
 
 _bootstart:
 # 调用 INT10-03h 中断 获取光标所在的 行和列，为输出字符提供参数
-# Int 10 service 0x03
-#   AH = 0x03
-#   BH = 0  Display page number / 显示页号 
-# Return:
-#   CH = Cursor start line / 光标起始列
-#   CL = Cursor End line   / 光标终止列
-#   DH = Row    / 行号
-#   DL = Column / 列号
-	xor %bh, %bh      # BH=0
+	xor %bh, %bh
 	mov $0x03, %ah    # 03H 号功能 读取光标位置信息
-	int $0x10         # INT 10H 中断 显示服务
-	                  # 返回: DX (DH 行, DL 列)
+	int $0x10         # INT 10H
 
 # 调用 INT10-1301h 中断 输出字符串
-# Int 10 service 0x13 subservice 0x01
-#   AH = 0x13
-#   AL = 0x01   Subservice (0~3)
-#   BH = Display page number/ 显示页
-#   BL = Color Attribute    / 字符颜色(CGA 标准)
-#   CX = Length of string   / 字符串长度
-#   DH = Row position     / 输出字符串`行`位置 
-#   DL = Column position  / 输出字符串`列`位置
-#   ES:BP = Pointer to string / 指向要输出的字符串
-# Return:
-#   None
-	xor %bh, %bh      # BH=0 页号
-	mov $0x2e, %bl    # BL 字符颜色 
-	mov $20, %cx      # CX 输出字符长度
+	xor %bh, %bh      
+	mov $0x2e, %bl    # BL 字符颜色
+	mov $18, %cx      # CX 输出字符长度
 # DL/DH 已通过 INT10-03h 获取
 	mov $BOOTSEG, %ax # 通过 AX 中转 BOOTSEG 的地址到ES
-	mov %ax, %es      # 设置好 ES 寄存器，为后续输出字符串准备 
+	mov %ax, %es
 	mov $msg1, %bp    # ES:BP 指向要输出的字符串
-	mov $0x1301, %ax  # write string, move cursor
+	mov $0x1301, %ax
 	int $0x10         # 调用中断 INT10
 
-loop:
-	jmp loop  # 死循环
+# 调用 INT13 - 02h 中断 读取磁盘
+# Int 13 service 0x02
+#   AH = 0x02
+#   AL = Number of sectors to read (must be nonzero) 
+#         / 想读取的扇区数 (非零)
+#   CH = low 8 bits of cylinder number 
+#   CL = sectors number 1-63 (bits 0-5) 
+#         high two bits of cylinder (bits 6-7, HDD only)
+#
+#       |  CH   |   CL  |
+#       |F|E|D-8|7|6|5-0|   CX
+#        | | | | | | | |
+#        | | | | | |  `---- ( 6 bits) sector number:    0~63
+#         `-`-`-`-`-`------ (10 bits) cylinder number:  0~1023
+#
+#   DH = head number  / 磁头号
+#   DL = drive number / 驱动器类型
+#      = 0x00 floppy A:, 
+#      = 0x01 floppy B:, 
+#      = 0x80 HDD 0,
+#      = 0x81 HDD 1
+#   ES:BX = Data buffer
+# Return:
+#   AH = status / 读取状态/错误码 
+#   AL = number of sectors read / 读取的扇区数
+#   CF = 0 if successful
+#      = 1 if error
+_load_demo:
+	mov $0x0002, %cx  # CX: 读取的扇区与柱面
+	mov $0x0000, %dx  # 0号磁头, 0号盘
+	mov $DEMOSEG, %ax # 中转$DEMO的地址
+	mov %ax, %es
+	mov $0x0200, %bx  # ED:BX 指向buffer
+	mov $0x02, %ah
+	mov $4, %al       # 读4个扇区
+	int $0x13         # INT0x13 - 0x02
+
+	jnc demo_load_ok  # 如果CF=0，读取成功，转跳
+	jmp _load_demo    # 读取不成功，则继续读取 
+
+demo_load_ok:
+	# Jump to the demo program
+	mov $DEMOSEG, %ax
+	mov %ax, %ds
+
+	# DO NOT USE: mov %AX, %CS  
+	ljmp $0x1020, $0 
 
 msg1:
 	.byte 13,10
 	.ascii "Hello woclass!"
-	.byte 13,10,13,10
+	.byte 13,10
 
-.= 510  #     这里是对齐语法 等价于 .org= 表示在该处补零
-        # 一直补到地址为 510 的地方 (即第一扇区的最后两字节)
-        #     然后在这里填充好0xaa55魔术值，BIOS会识别硬盘中
-        # 第一扇区以0xaa55结尾的为启动扇区，于是BIOS会装载代
-        # 码并且运行
+.= 510 # 0填充对齐 
 
 signature:
 	.word 0xAA55
